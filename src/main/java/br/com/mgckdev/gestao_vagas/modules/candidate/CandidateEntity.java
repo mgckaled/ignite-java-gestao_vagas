4df2aefd5447a63b6266ddddddf71cbeefb97339
mgckaled <marcelgckaled@gmail.com>
2023-10-25 11:08:19 -0300

    feat: add POST request data validation

--- src/main/java/br/com/mgckdev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/mgckdev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,18 +2,27 @@ package br.com.mgckdev.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CandidateEntity {
-  
+
   private UUID id;
   private String name;
+
+  @Pattern(regexp = "^(?!\\s*S).+", message = "O campo [username] não deve conter espaços")
   private String username;
+
+  @Email(message = "O campo [e-mail] deve conter um e-mail válido")
   private String email;
+
+  @Length(min = 10, max = 50)
   private String password;
   private String description;
   private String curriculum;
 
-  
 }
