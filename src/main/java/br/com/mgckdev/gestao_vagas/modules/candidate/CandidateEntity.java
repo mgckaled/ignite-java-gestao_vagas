916a363166a1c1782dfa6f42ed2bcfcdec4189c2
mgckaled <marcelgckaled@gmail.com>
2023-10-26 11:24:38 -0300

    config: create candidate table

--- src/main/java/br/com/mgckdev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/mgckdev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,19 +1,31 @@
 package br.com.mgckdev.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
+  @NotBlank
   @Pattern(regexp = "\\S+", message = "O campo não deve conter espaços")
   private String username;
 
@@ -25,4 +37,6 @@ public class CandidateEntity {
   private String description;
   private String curriculum;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
