be5b660058e1f1636055dc2ca4fa2748dcd76d69
mgckaled <marcelgckaled@gmail.com>
2023-11-22 10:08:25 -0300

    test: insert in-memory database for jobs

--- src/test/java/br/com/mgckdev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/mgckdev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,7 +1,5 @@
 package br.com.mgckdev.gestao_vagas.modules.company.controllers;
 
-import java.util.UUID;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,43 +17,55 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import br.com.mgckdev.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.mgckdev.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.mgckdev.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.mgckdev.gestao_vagas.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
-    
+
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
 
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @Before
-    public void setup(){
+    public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(context)
-            .apply(SecurityMockMvcConfigurers.springSecurity())
-            .build();
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
     }
 
     @Test
-    public void should_be_able_to_create_a_new_job() throws Exception{
+    public void should_be_able_to_create_a_new_job() throws Exception {
 
+        var company = CompanyEntity.builder()
+                .description("COMPANY_DESCRIPTION")
+                .email("email@company.com")
+                .password("1234567890")
+                .username("COMPANY_USERNAME")
+                .name("COMPANY_NAME").build();
+
+        company = companyRepository.saveAndFlush(company);
 
         var createdJobDTO = CreateJobDTO.builder()
-        .benefits("BENEFITS_TEST")
-        .description("DESCRIPTION_TEST")
-        .level("LEVEL_TEST")
-        .build();
+                .benefits("BENEFITS_TEST")
+                .description("DESCRIPTION_TEST")
+                .level("LEVEL_TEST")
+                .build();
 
         var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(TestUtils.objectToJson(createdJobDTO))
-            .header("Authorization", TestUtils.generateToken(UUID.fromString("336e635e-0918-48c2-b7bb-b3ba21a1dfd0"), "JAVAGAS_@123#"))
-            )
-            .andExpect(MockMvcResultMatchers.status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectToJson(createdJobDTO))
+                .header("Authorization", TestUtils.generateToken(company.getId(), "JAVAGAS_@123#")))
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
         System.out.println(result);
     }
 
-    
 }
